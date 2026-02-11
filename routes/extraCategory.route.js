b4a2b0bfbe896320ce3261edfc4ab7ec83578ea5
HardikSoni1994 <soni.hardik994@gmail.com>
2026-02-12 00:36:41 +0530

    extraCategory CRUD file path, route and .ejs completion

--- routes/extraCategory.route.js
+++ routes/extraCategory.route.js
@@ -6,5 +6,9 @@ const extraCategoryController = require('../controllers/extraCategory.controller
 // Routes
 router.get('/addExtraCategory', extraCategoryController.addExtraCategoryPage);
 router.post('/insertExtraCategory', extraCategoryController.insertExtraCategory);
+router.get('/viewExtraCategory', extraCategoryController.viewExtraCategoryPage);
+router.get('/deleteExtracategory/:id', extraCategoryController.deleteExtraCategory);
+router.get('/editExtraCategory/:id', extraCategoryController.editExtraCategory);
+router.post('/updateExtraCategory', extraCategoryController.updateExtraCategory);
 
 module.exports = router;
